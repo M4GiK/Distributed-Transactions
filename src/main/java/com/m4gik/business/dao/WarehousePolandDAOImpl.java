14b1d1b96e17a9e0b62d8da2c36a353ce244a18f
m4gik <michal.szczygiel@wp.pl>
2014-01-02 15:44:39 +0100

    Add implementation for WarehousePolandDAO

---
+++ src/main/java/com/m4gik/business/dao/WarehousePolandDAOImpl.java
@@ -0,0 +1,37 @@
+/**
+ * Project Distributed Transactions.
+ * Copyright Michał Szczygieł, 2013.
+ * Created at Jan 2, 2014.
+ */
+package com.m4gik.business.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.m4gik.business.model.WarehousePoland;
+
+/**
+ * 
+ * This class represent implementation for Data Access Object.
+ * 
+ * @author m4gik <michal.szczygiel@wp.pl>
+ * 
+ */
+public class WarehousePolandDAOImpl implements WarehousePolandDAO {
+
+    @PersistenceContext(unitName = "PersistenceUnitWarehousePoland")
+    private EntityManager entityManager;
+
+    /**
+     * The persistence context needed to persist entity identity as a unique
+     * entity instance. This method overrides an existing method.
+     * 
+     * @see com.m4gik.business.dao.WarehousePolandDAO#persistEmployee(com.m4gik.business.model.WarehousePoland)
+     */
+    @Override
+    public void persistEmployee(WarehousePoland warehousePoland)
+            throws Exception {
+        entityManager.persist(warehousePoland);
+    }
+
+}
